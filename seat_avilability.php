26d0a77de8170d43e8ed086647f4d259154e2b77
Dablu <dablu.kumar007@gmail.com>
2013-10-31 21:51:46 -0400

    Seat avilability file added

---
+++ seat_avilability.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Indian Railways</title>
+<meta name="keywords" content="Green Black, Web Design" />
+<meta name="description" content="Green Black " />
+<link href="templatemo_style1.css" rel="stylesheet" type="text/css" />
+<script language="javascript" type="text/javascript">
+function clearText(field)
+{
+    if (field.defaultValue == field.value) field.value = '';
+    else if (field.value == '') field.value = field.defaultValue;
+}
+</script>
+</head>
+<body>
+<div id="templatemo_container_wrapper">
+<div id="templatemo_container">
+	<div id="templatemo_banner">
+  <div id="site_title">
+			<a href="#"><img  src="images/rail_logo_new_red.gif" alt="image one" /></a>
+            <p><a href="http://www.templatemo.com" target="_parent"> <span>INDIAN RAILWAYS PASSENGER RESERVATION ENQUIRY</span></a></p>
+      </div>
+        
+        <div id="templatemo_menu">
+            <ul>
+                <li><a href="railway.html" class="current">Home</a></li>
+                <li><a href="pnrenquiry.php" target="_parent">PNR Status</a></li>
+                <li><a href="findtrains.php">Train Between Important Stations</a></li>
+                <li><a href="seat_avilability.php">Seat Availability</a></li>
+                <li><a href="fare_enquiry.php">Fare Enquiry</a></li>
+                <li><a href="#">Internet Reservation</a></li>
+            </ul>
+		</div> <!-- end of menu -->
+    
+    </div> <!-- end of banner -->
+    
+    <div id="templatemo_content">
+    	
+        <div id="side_column">
+        
+        	<div class="side_column_box">
+            
+            	<h2><span></span>Services</h2>
+                
+                <div class="side_column_box_content">
+                	<ul>
+						<li><a href="#" class="current">Availability at Major Stations</a></li>
+						<li><a href="http://www.templatemo.com" target="_parent">Train Schedule</a></li>
+						<li><a href="#">Tatkal Scheme</a></li>
+						<li><a href="http://www.koflash.com" target="_blank">Upgraded Passenger Scheme</a></li>
+						<li><a href="#">SMS Service</a></li>
+						<li><a href="#">Train Berth Availability</a></li>
+				</ul>
+					
+					
+                </div>
+                
+                <div class="bottom"></div>
+            </div>
+            
+            <div class="side_column_box">
+            
+            	<h2><span></span>Information</h2>
+                
+                <div class="side_column_box_content">
+						<ul>	
+						<li><a href="#" class="current">Train Running Information</a></li>
+						<li><a href="http://www.templatemo.com" target="_parent">Train Type Information</a></li>
+						<li><a href="#">View Codes</a></li>
+						<li><a href="http://www.koflash.com" target="_blank">Trains at a Glance</a></li>
+						<li><a href="#">Rules</a></li>
+						<li><a href="#">International Tourists</a></li>
+						<li><a href ="#">Other Railway Websites</a></li>
+                   </ul>
+                    <div class="cleaner"></div>
+                </div>
+                
+                <div class="bottom"></div>
+            </div>
+            
+        
+        </div> <!-- end of side column -->
+        
+        <div id="main_column">
+        
+            <div class="main_column_section">       
+            
+               
+                
+                <div class="main_column_section_content">
+                
+                  <td><b><u><center>
+<p>Accommodation Availability Enquiry on Trains till Departure<center></p>
+</u></b><br>
+<center>
+Train Number :<input type="text" size =17%><br><br>
+Journey Date: <input type="text" size =5%>  <input type="text" size =5%><br><br>
+Source Stn. Code :<input type="text" size =5%>
+Destination Stn. Code:<input type="text" size = 5%><br><br>
+Class:<select name="Class">
+<option value="select one">-----------Class-----------</option>
+<option value="ALL CLASS">ALL CLASS</option>
+<option value="FIRST AC">FIRST AC</option>
+<option value="SECOND AC">SECOND AC</option>
+<option value="THIRD AC">THIRD AC</option>
+<option value="3 AC Economy">3 AC Economy</option>
+<option value="AC CHAIR CAR">AC CHAIR CAR</option>
+<option value="FIRST CLASS">FIRST CLASS</option>
+<option value="SECOND SEATING">SECOND SEATING</option>
+<option value="SLEEPER CLASS">SLEEPER CLASS</option>
+</select>
+Quota :<select name="Quota">
+<option value="select one">--------------------Quota--------------------</option>
+<option value="GENERAL QUOTA">GENERAL QUOTA</option>
+<option value="TATKAL QUOTA">TATKAL QUOTA</option>
+<option value="LADIES QUOTA">LADIES QUOTA</option>
+<option value="DEFENCE QUOTA">DEFENCE QUOTA</option>
+<option value="YUVA QUOTA">YUVA QUOTA</option>
+<option value="FOREIGN TOURIST QUOTA">FOREIGN TOURIST QUOTA</option>
+<option value="DUTY PASS QUOTA">DUTY PASS QUOTA</option>
+<option value="PARLIAMENT HOUSE QUOTA">PARLIAMENT HOUSE QUOTA</option>
+<option value="HANDICAPED QUOTA">HANDICAPED QUOTA</option>
+<option value="LOWER BERTH QUOTA">LOWER BERTH QUOTA</option>
+</select>
+<br><br>   
+<input type="submit" value="Get Availability ">
+<input type="submit" value="Clear"></center>
+</td>
+
+</tr>
+</table>
+
+                  </div>
+              <div class="cleaner"></div>
+                <div class="bottom"></div>
+            </div>
+            
+           
+        
+        	
+            <div class="cleaner"></div>
+        </div> <!-- end of main column -->
+    
+    	<div class="cleaner"></div>
+    </div> <!-- end of content -->
+    
+    <div id="templatemo_footer">
+    
+    <div id="templatemo_footer_bar">
+    
+        <ul class="footer_menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="http://www.templatemo.com" target="_parent">Ministry of Railways</a></li>
+          <li><a href="#">Trains between Stations</a></li>
+            <li><a href="#">Booking Locations</a></li>
+            <li><a href="#">CRIS</a></li>
+            <li class="last_menu"><a href="#">CONCERT</a></li>
+			<li class="last_menu"><a href="#">Advertise with CRIS</a></li>
+			<li class="last_menu"><a href="#">Railway Map</a></li>
+			<li class="last_menu"><a href="#">FAQ</a></li>
+			<li class="last_menu"><a href="#">Sitemap</a></li>
+			<li class="last_menu"><a href="#">Feedback</a></li>
+        </ul>
+
+        Copyright ?2013,<a href="#">Center For Indian Railway Information System</a> | <a href="http://www.iwebsitetemplate.com" target="_parent">Designed and hosted</a> by <a href="http://www.templatemo.com" target="_parent">Dablu kumar</a>
+        
+    </div>
+    
+    </div> <!-- end of footer -->
+
+</div> <!-- end of container -->
+</div> <!-- end of container wrapper -->
+
+</html>
